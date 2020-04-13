2e8838226ceeb291fc2068e3ff874418c73eb87b
martinvandoesburg93 <60740183+martinvandoesburg93@users.noreply.github.com>
2020-04-13 20:03:58 +0200

    Add files via upload

---
+++ ProjectB/Data/Data.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Security.Principal;
+
+namespace ProjectB.Data
+{
+	static class Data
+	{
+		public static event EventHandler<EventArgs> LanguageChanged;
+
+		private static Language _Lang = Language.eng;
+		
+		public static Language Lang
+		{
+			get => _Lang;
+			set
+			{
+				_Lang = value;
+				LanguageChanged?.Invoke(null, null);
+			}
+		}
+
+		public static Dish GetDishByCode(int code) 
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load("Dishes.xml");
+			XmlNode node = doc.SelectSingleNode($@"./Dishes/Dish[@type=""{code}""]");
+			return new Dish(code, node.SelectSingleNode($"./Name/{_Lang}").InnerText, node.SelectSingleNode($"./Desc/{_Lang}").InnerText, (Type)Enum.Parse(typeof(Type), node.Attributes["type"].Value), decimal.Parse(node.Attributes["price"].Value));
+		}
+	}
+
+	enum Language
+	{
+		eng,
+		nl
+	}
+}
---
+++ ProjectB/Data/Dish.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB.Data
+{
+	class Dish
+	{
+        public int DishCode;
+        public string Name;
+        public string Desc;
+        public Type Type;
+        public decimal Price;
+
+        public Dish() { }
+
+        public Dish(int dishcode, string name, string desc, Type type, decimal price)
+        {
+            DishCode = dishcode;
+            Name = name;
+            Desc = desc;
+            Type = type;
+            Price = price;
+        }
+    }
+
+    enum Type
+    {
+        Vega,
+        Vis,
+        Vlees,
+    }
+}
